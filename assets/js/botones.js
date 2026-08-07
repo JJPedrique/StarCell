a4fe43faae4d60d76abbb29851fab5c828f517ea
JJ_Pedrique: Intento 2 <pedriquej9@gmail.com>
2026-08-07 17:50:51 -0400

    JJ_Pedrique:retoques

--- assets/js/botones.js
+++ assets/js/botones.js
@@ -5,14 +5,14 @@ function irArriba(){
 }
 
 function preguntarProducto(telefono, producto) {
-  let mensaje = `Hola, ¿Me podria decir la disponibilidad y el precio del siguiente producto: ${producto}?`;
+  let mensaje = `Hola, ¿Me podría decir la disponibilidad y el precio del siguiente producto: ${producto}?`;
   let mensajeCodificado = encodeURIComponent(mensaje);
   let url = `https://wa.me/${telefono}?text=${mensajeCodificado}`;
   window.open(url, '_blank');
 }
 
 function preguntarServicio(telefono, producto) {
-  let mensaje = `Hola, ¿Me podria decir la disponibilidad y el precio del siguiente servicio: ${producto}?`;
+  let mensaje = `Hola, ¿Me podría decir la disponibilidad y el precio del siguiente servicio: ${producto}?`;
   let mensajeCodificado = encodeURIComponent(mensaje);
   let url = `https://wa.me/${telefono}?text=${mensajeCodificado}`;
   window.open(url, '_blank');
